42680cb6b68106d6b23bde3950a72185462ef928
RyanSoftwareDev <gavin.ryan.amtsec@gmail.com>
2026-01-07 09:21:44 -0500

    misc.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { Analytics } from "@vercel/analytics/next"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
